5936f9bc06cf8726fbbe4e897056280177587108
KateMila <89808903+KateMila@users.noreply.github.com>
2021-11-08 11:25:26 +0300

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int ticket_price = 6000;
-        int mile_price = 20;
+        int price = 6000;
+        int miles = 20;
 
-        int miles_bonus =ticket_price/mile_price;
-        System.out.println(miles_bonus);
+        int bonus = price / miles;
+        System.out.println(bonus);
     }
 }
